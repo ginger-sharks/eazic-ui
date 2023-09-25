410db7b144d9c5937a55ff2bbf6aca0144b346bc
jeansaigne <aymerictibere@gmail.com>
2023-09-25 16:46:03 +0200

    feat: Added code & style linting steps in cicd Fixes #6

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  plugins: [
+    "stylelint-scss"
+  ],
+  extends: "stylelint-config-recommended-vue/scss",
+  rules: {
+    'at-rule-empty-line-before': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': null,
+    'declaration-empty-line-before': null,
+    'rule-empty-line-before': null,
+    'selector-list-comma-newline-after': null,
+    'unit-disallowed-list': ['px']
+  },
+};
