7da16b479758abdb567de6ea0587bce0abbd3a60
Aymeric Tibere <aymerictibere@gmail.com>
2023-10-23 11:27:48 +0200

    fix: fixed typing problems concerning okta usage

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import { OktaAuth } from '@okta/okta-auth-js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -7,20 +8,29 @@ import App from './App.vue'
 import router from './router'
 
 import OktaVue from '@okta/okta-vue'
-import { oktaAuth } from './okta';
 
 const app = createApp(App)
+
+const yourOktaUri = 'https://dev-77536915.okta.com'
+const clientId = '0oabnu0zcoU50WOG85d7'
+
+const oktaAuth = new OktaAuth({
+  issuer: `https://dev-77536915.okta.com/oauth2/default`,
+  clientId: clientId,
+  redirectUri: window.location.origin + '/login/callback',
+  scopes: ['openid', 'profile', 'email']
+})
 
 app.use(createPinia())
 app.use(router)
 app.use(OktaVue, {
-        oktaAuth,
-        onAuthRequired: () => {
-            router.push('/login')
-        },
-        onAuthResume: () => {
-            router.push('/login')
-        },
-    })
+  oktaAuth,
+  onAuthRequired: () => {
+    router.push('/login')
+  },
+  onAuthResume: () => {
+    router.push('/login')
+  }
+})
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <form @submit.prevent="search">
-      <input type="text" placeholder="search..." v-model="keywords">
+      <input type="text" placeholder="search..." v-model="keywords" />
     </form>
     <pre>
       {{ JSON.stringify(result, undefined, 2) }}
@@ -11,28 +11,27 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import {oktaAuth} from '../okta'
-import axios from 'axios';
+import { useAuth } from '@okta/okta-vue'
+import { ref } from 'vue'
+import axios from 'axios'
 
-const keywords = ref<string>("");
-const result = ref<any[]>([]);
-const errore = ref<any>();
+const keywords = ref<string>('')
+const result = ref<any[]>([])
+const errore = ref<any>()
+const auth = useAuth()
 
 async function search() {
-  console.log("searching with " + keywords.value);
-  let accessToken = oktaAuth.getAccessToken();
+  console.log('searching with ' + keywords.value)
+  let accessToken = auth.getAccessToken()
   try {
-    let response = await axios.get('http://localhost:8082/youtube/search/' + keywords.value,
-      { headers: { 'Authorization': 'Bearer ' + accessToken } });
-    result.value = response.data;
-    console.log(result.value);
-  }
-  catch (error) {
+    let response = await axios.get('http://localhost:8082/youtube/search/' + keywords.value, {
+      headers: { Authorization: 'Bearer ' + accessToken }
+    })
+    result.value = response.data
+    console.log(result.value)
+  } catch (error) {
     errore.value = `${error}`
-    console.log(errore.value);
+    console.log(errore.value)
   }
 }
-
-
 </script>
